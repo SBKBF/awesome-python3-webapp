37733ea6076a8b73b00e4d24673792c8b732fc79
lyh <772540917@qq.com>
2017-05-13 10:08:41 +0800

    first

--- www/test.py
+++
@@ -1,9 +0,0 @@
-from www.coroweb import get
-from www.models import User
-@get('/')
-def index(request):
-    users = yield from User.findAll()
-    return {
-        '__template__': 'test.html',
-        'users': users
-    }
